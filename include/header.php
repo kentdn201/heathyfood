ff8144a9fdc649d3ae152108724ffce8fa33a056
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-12-29 19:18:49 +0700

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,5 +1,10 @@
 <?php
-include("../shared_assets/conn.php");
+
+    $conn = pg_connect("host=ec2-3-217-170-198.compute-1.amazonaws.com user=epfhbsltcnedlv dbname=d1a3vgbq801lf1 password=3a4dd46548f9a48d9a19bdc2675ac1f01888b0f6a559498566ed31ad7f3330e5");
+    if(!$conn){
+        die("Connect failed: " .pg_connect_error());
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="zxx">
