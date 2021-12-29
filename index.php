c14a1660d219c75a2e794e78e5a5955df4c23e17
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-12-29 18:16:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-	include("../shared_assets/conn.php");
+    $servername = "ec2-3-217-170-198.compute-1.amazonaws.com";
+    $username = "epfhbsltcnedlv";
+    $password = "3a4dd46548f9a48d9a19bdc2675ac1f01888b0f6a559498566ed31ad7f3330e5";
+    $database = "d1a3vgbq801lf1";
+
+    $conn = pg_connect($servername, $username, $password, $database);
+
+    if($conn){
+        echo "Connect thành công";
+    }
+
+    if(!$conn){
+        die("Connect failed: " .pg_connect_error());
+    }
+
 ?>
