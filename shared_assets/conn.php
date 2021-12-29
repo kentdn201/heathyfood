caaaa714dc5529a2a0633e0df964abedbeb73e0e
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-12-29 17:57:59 +0700

    Update conn.php

--- shared_assets/conn.php
+++ shared_assets/conn.php
@@ -1,10 +1,10 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "crp_final";
+    $servername = "ec2-3-217-170-198.compute-1.amazonaws.com";
+    $username = "epfhbsltcnedlv";
+    $password = "3a4dd46548f9a48d9a19bdc2675ac1f01888b0f6a559498566ed31ad7f3330e5";
+    $database = "d1a3vgbq801lf1";
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = pg_connect($servername, $username, $password, $database);
 
     if(!$conn){
         die("Connect failed: " .mysqli_connect_error());
