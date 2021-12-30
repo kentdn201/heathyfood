da68ae92ef498031140f7e4e96309ca7de9c899b
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2021-12-30 15:58:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 	include("include/header.php");
+	    $conn = pg_connect("host=ec2-3-217-170-198.compute-1.amazonaws.com user=epfhbsltcnedlv dbname=d1a3vgbq801lf1 password=3a4dd46548f9a48d9a19bdc2675ac1f01888b0f6a559498566ed31ad7f3330e5");
+	    if(!$conn){
+		die("Connect failed: " .pg_connect_error());
+	    }
 	$user = (isset($_SESSION['user']))? $_SESSION['user'] : [];
 ?>
 	<!-- top Products -->
