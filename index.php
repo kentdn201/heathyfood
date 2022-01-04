972631e522f814465a6ac56c62734a7989929d09
kentdn201 <76418325+kentdn201@users.noreply.github.com>
2022-01-04 10:12:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -47,8 +47,8 @@
 							<?php } ?>
 							</div>
 							<div class="col-xs-8 img-deal1">
-								<h3><?= echo $row['productName']?></h3>
-								<a href="single.php?id=<?php $row['productID']?>"><?= echo $row['productPrice']?></a>
+								<h3><?= $row['productName']?></h3>
+								<a href="single.php?id=<?= $row['productID']?>"><?= $row['productPrice']?></a>
 							</div>
 						<div class="clearfix"></div>
 						</a>
